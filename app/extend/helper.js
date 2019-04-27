04fc4cfe0daf248d46789b1bb7eed17782e0d22c
yuanyunlong <yunyunlong@192.168.3.15>
2019-04-28 07:34:46 +0800

    first

---
+++ app/extend/helper.js
@@ -0,0 +1,14 @@
+const moment = require('moment');
+
+// 格式化事件
+exports.formatTime  = time => moment(time).format('YYYY-MM-DD HH:mm:ss')
+
+//处理成功响应
+exports.success = ({ctx, res=null, msg='请求成功'}) => {
+    ctx.body = {
+        code:0,
+        date:res,
+        msg
+    }
+    ctx.status = 200;
+}
